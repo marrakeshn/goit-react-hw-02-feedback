63796bbf50e34781074429b047c9a99728b91016
marrakeshn <nesveteymark@gmail.com>
2023-04-07 11:58:48 +0300

    complete part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import s from './App.module.css';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addGood = () => {
+    this.setState(state => ({
+      good: state.good + 1,
+    }));
+  };
+
+  addNeutral = () => {
+    this.setState(state => ({
+      neutral: state.neutral + 1,
+    }));
+  };
+
+  addBad = () => {
+    this.setState(state => ({
+      bad: state.bad + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div className={s.container}>
+        <p>Please leave feedback</p>
+        <button onClick={this.addGood}>Good</button>
+        <button onClick={this.addNeutral}>Neutral</button>
+        <button onClick={this.addBad}>Bad</button>
+        <p>Statistics</p>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
